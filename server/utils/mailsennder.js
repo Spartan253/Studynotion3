164808f4415e51035d5cf2d2ed7b074a6532cbb2
Spartan 253 <ayushkhoke1@gmail.com>
2026-01-09 16:17:27 +0530

    Fix nodemailer production config

--- server/utils/mailsennder.js
+++ server/utils/mailsennder.js
@@ -1,61 +1,28 @@
-// const nodemailer = require('nodemailer');
-
-// const mailsender = async (email, title, body) => {
-//     try {
-//         let transporter = nodemailer.createTransport({
-//             host: process.env.MAIL_HOST,
-//             auth: {
-//                 user: process.env.MAIL_USER,
-//                 pass: process.env.MAIL_PASS,
-//             }
-//         })
-
-//         let info = await transporter.sendMail({
-//             from: "studynotion || codehelp bybabbber",
-//             to: `${email}`,
-//             subject: `${title}`,
-//             html: `${body}`,
-//         })
-//         console.log(info);
-//         return info;
-//     }
-//     catch (error) {
-//         console.log(error.message);
-//     }
-// }
-
-
-// module.exports = mailsender;
-
-
-
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 const mailsender = async (email, title, body) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      port: 587,              // IMPORTANT
-      secure: false,          // IMPORTANT (true only for 465)
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-      },
-    });
+    try {
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            }
+        })
 
-    const info = await transporter.sendMail({
-      from: `"StudyNotion" <${process.env.MAIL_USER}>`,
-      to: email,
-      subject: title,
-      html: body,
-    });
+        let info = await transporter.sendMail({
+            from: "studynotion || codehelp bybabbber",
+            to: `${email}`,
+            subject: `${title}`,
+            html: `${body}`,
+        })
+        console.log(info);
+        return info;
+    }
+    catch (error) {
+        console.log(error.message);
+    }
+}
 
-    console.log("Email sent:", info.messageId);
-    return info;
-  } catch (error) {
-    console.error("Mail error:", error);
-    throw error; // VERY IMPORTANT
-  }
-};
 
 module.exports = mailsender;
