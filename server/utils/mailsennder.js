4469b7cf94af1e16a721f14cfa846c602e664cd5
Spartan 253 <ayushkhoke1@gmail.com>
2026-01-11 14:34:55 +0530

    Fix the code

--- server/utils/mailsennder.js
+++ server/utils/mailsennder.js
@@ -5,7 +5,6 @@ const mailsender = async (email, title, body) => {
         let transporter = nodemailer.createTransport({
             host: process.env.MAIL_HOST,
              port: 587,
-            secure: false,
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
