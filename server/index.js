e3b802a79a09860fae7e552f1a73d9024808603c
Spartan 253 <ayushkhoke1@gmail.com>
2026-01-12 17:14:25 +0530

    all errors are fixed now

--- server/index.js
+++ server/index.js
@@ -90,7 +90,7 @@ database.connect();
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
-  "https://studynotion3-ylbj.vercel.app/",
+  "https://studynotion3-ylbj.vercel.app",
 ];
 
 app.use(
